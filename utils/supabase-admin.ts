3624c241a2788fa9b73f24d9842c240b0b53b359
Kye <kye@apacmediasolutions.com>
2023-03-30 14:49:45 -0400

    supabase service role key

--- utils/supabase-admin.ts
+++ utils/supabase-admin.ts
@@ -1,7 +1,11 @@
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
 import { createClient } from '@supabase/supabase-js';
+import { Database } from 'types/types_db';
 
 const supabaseAdmin = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL || '',
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
 );
   
+
+export const supabase = createBrowserSupabaseClient<Database>();
