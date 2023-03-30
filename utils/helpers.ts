cd129ece5789a4a41e9ca6760328825ef42b3a7b
Kye <kye@apacmediasolutions.com>
2023-03-30 15:35:35 -0400

    get url

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -6,7 +6,7 @@ export const getURL = () => {
         process?.env?.PUBLIC_VERCEL_URL ?? 
         'http://localhost:3000/';
     //make sure to include https://
-    url = url.includes('http') ? url : `https://${url}`;
+    // url = url.includes('http') ? url : `https://${url}`;
 
     url = url.charAt(url.length - 1 ) === '/' ? url : `${url}/`;
     return url
