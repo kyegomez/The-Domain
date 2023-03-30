2f25e1ee0ff4b0fe94603c19039c5e41dcea024d
Kye <kye@apacmediasolutions.com>
2023-03-30 14:18:51 -0400

    useSupaBaseClient() error

--- components/layout/sign-in-modal.tsx
+++ components/layout/sign-in-modal.tsx
@@ -16,7 +16,7 @@ import { ThemeSupa } from '@supabase/auth-ui-shared';
 import { useUser, useSupabaseClient } from '@supabase/auth-helpers-react';
 import { Auth } from '@supabase/auth-ui-react';
 import { createClient } from "@supabase/supabase-js";
-
+import {Logo} from '@/components/signUI/Logo'
 
 
 const supa_url: any = process.env.NEXT_PUBLIC_SUPABASE_URL;
@@ -31,7 +31,8 @@ const SignInModal = ({
 }) => {
   const router = useRouter();
   const user = useUser();
-  const supabaseClient = createClient(supa_url, supa_key);
+  // const supabaseClient = createClient(supa_url, supa_key);
+  const supabaseClient = useSupabaseClient()
 
   useEffect(() => {
     if (user) {
@@ -44,19 +45,12 @@ const SignInModal = ({
       <Modal showModal={showSignInModal} setShowModal={setShowSignInModal}>
         <div className="w-full overflow-hidden shadow-xl md:max-w-md md:rounded-2xl md:border md:border-gray-200">
           <div className="flex flex-col items-center justify-center space-y-3 border-b border-gray-200 bg-white px-4 py-6 pt-8 text-center md:px-16">
-            <a href="https://precedent.dev">
-              <Image
-                src="/logo.png"
-                alt="Logo"
-                className="h-10 w-10 rounded-full"
-                width={20}
-                height={20}
-              />
+            <a href="https://landing.apac.ai">
+              <Logo />
             </a>
             <h3 className="font-display text-2xl font-bold">Sign In</h3>
             <p className="text-sm text-gray-500">
-              This is strictly for demo purposes - only your email and profile
-              picture will be stored.
+              Log in to your digital spirit.
             </p>
           </div>
 
